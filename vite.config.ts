302ca0597a19d41f56159437366fb4734e44fc99
StevenWu <1055034062@qq.com>
2023-02-24 15:17:13 +0800

    引入element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,29 @@
  * @Autor: StevenWu
  * @Date: 2023-02-23 20:26:41
  * @LastEditors: StevenWu
- * @LastEditTime: 2023-02-24 09:54:20
+ * @LastEditTime: 2023-02-24 15:14:52
  */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-// const vetur = require('@volar-plugins/vetur"');
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
