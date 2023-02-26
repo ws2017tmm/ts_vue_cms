dc2ca181a63823db4ee08bfd5a5a21258092d79d
StevenWu <1055034062@qq.com>
2023-02-26 21:19:24 +0800

    登录路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,48 +4,8 @@
  * @Autor: StevenWu
  * @Date: 2023-02-23 20:26:41
  * @LastEditors: StevenWu
- * @LastEditTime: 2023-02-26 21:06:43
+ * @LastEditTime: 2023-02-26 21:15:46
  */
-import { createRouter, createWebHashHistory } from 'vue-router'
-import { LOGIN_TOKEN } from '@/global/constants'
-import { localCache } from '@/utils/cache'
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [
-    {
-      path: '/',
-      redirect: '/main'
-    },
-    {
-      path: '/main',
-      name: 'main',
-      component: () => import('../views/mian/Main.vue')
-    },
-    {
-      path: '/login',
-      name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/login/Login.vue')
-    }
-  ]
-})
-
-const whiteList = ['/login']
-router.beforeEach((to, from, next) => {
-  console.log(to, from, next, '---beforeEach')
-  const token = localCache.getCache(LOGIN_TOKEN)
-  if (token) {
-    next()
-  } else {
-    if (to.path === '/login' || whiteList.indexOf(to.path) !== -1) {
-      next()
-    } else {
-      next('/login')
-    }
-  }
-})
+import router from './login/login'
 
 export default router
--- src/router/login/login.ts
+++ src/router/login/login.ts
@@ -4,22 +4,25 @@
  * @Autor: StevenWu
  * @Date: 2023-02-26 20:43:12
  * @LastEditors: StevenWu
- * @LastEditTime: 2023-02-26 21:11:06
+ * @LastEditTime: 2023-02-26 21:17:40
  */
-// import router from '../index'
-// import { LOGIN_TOKEN } from '@/global/constants'
+import router from '../route'
+import { LOGIN_TOKEN } from '@/global/constants'
+import { localCache } from '@/utils/cache'
 
-// const whiteList = ['/login']
-// router.beforeEach((to, from, next) => {
-//   console.log(to, from, next, '---beforeEach')
-//   const token = localCache.getCache(LOGIN_TOKEN)
-//   if (token) {
-//     next()
-//   } else {
-//     if (to.path === '/login' || whiteList.indexOf(to.path) !== -1) {
-//       next()
-//     } else {
-//       next('/login')
-//     }
-//   }
-// })
+const whiteList = ['/login']
+router.beforeEach((to, from, next) => {
+  console.log(to, from, next, '---beforeEach')
+  const token = localCache.getCache(LOGIN_TOKEN)
+  if (token) {
+    next()
+  } else {
+    if (to.path === '/login' || whiteList.indexOf(to.path) !== -1) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})
+
+export default router
