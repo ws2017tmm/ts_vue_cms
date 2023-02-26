96cf2235e242de653978aec3d1fbc2d208192f6c
StevenWu <1055034062@qq.com>
2023-02-26 21:11:12 +0800

    登录路由守卫

--- src/router/login/login.ts
+++ src/router/login/login.ts
@@ -4,11 +4,22 @@
  * @Autor: StevenWu
  * @Date: 2023-02-26 20:43:12
  * @LastEditors: StevenWu
- * @LastEditTime: 2023-02-26 20:56:33
+ * @LastEditTime: 2023-02-26 21:11:06
  */
 // import router from '../index'
 // import { LOGIN_TOKEN } from '@/global/constants'
 
-// router.beforeEach((to, from) => {
-//   console.log(to, from, '---beforeEach')
+// const whiteList = ['/login']
+// router.beforeEach((to, from, next) => {
+//   console.log(to, from, next, '---beforeEach')
+//   const token = localCache.getCache(LOGIN_TOKEN)
+//   if (token) {
+//     next()
+//   } else {
+//     if (to.path === '/login' || whiteList.indexOf(to.path) !== -1) {
+//       next()
+//     } else {
+//       next('/login')
+//     }
+//   }
 // })
