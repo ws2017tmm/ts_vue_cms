59680f99c3dec522065cf5ea9e002a18d219af4a
StevenWu <1055034062@qq.com>
2023-02-26 21:07:33 +0800

    登录路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,11 @@
  * @Autor: StevenWu
  * @Date: 2023-02-23 20:26:41
  * @LastEditors: StevenWu
- * @LastEditTime: 2023-02-26 20:37:05
+ * @LastEditTime: 2023-02-26 21:06:43
  */
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { LOGIN_TOKEN } from '@/global/constants'
+import { localCache } from '@/utils/cache'
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -29,6 +31,21 @@ const router = createRouter({
       component: () => import('../views/login/Login.vue')
     }
   ]
+})
+
+const whiteList = ['/login']
+router.beforeEach((to, from, next) => {
+  console.log(to, from, next, '---beforeEach')
+  const token = localCache.getCache(LOGIN_TOKEN)
+  if (token) {
+    next()
+  } else {
+    if (to.path === '/login' || whiteList.indexOf(to.path) !== -1) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
 })
 
 export default router
---
+++ src/router/login/login.ts
@@ -0,0 +1,14 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Autor: StevenWu
+ * @Date: 2023-02-26 20:43:12
+ * @LastEditors: StevenWu
+ * @LastEditTime: 2023-02-26 20:56:33
+ */
+// import router from '../index'
+// import { LOGIN_TOKEN } from '@/global/constants'
+
+// router.beforeEach((to, from) => {
+//   console.log(to, from, '---beforeEach')
+// })
