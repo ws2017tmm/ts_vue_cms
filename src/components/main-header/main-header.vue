391d53b44aaae0523938aff6cd6fe3d488d06cbf
StevenWu <1055034062@qq.com>
2023-02-27 11:10:59 +0800

    公共头部

--- src/components/main-header/main-header.vue
+++ src/components/main-header/main-header.vue
@@ -4,16 +4,56 @@
  * @Autor: StevenWu
  * @Date: 2023-02-26 22:02:14
  * @LastEditors: StevenWu
- * @LastEditTime: 2023-02-26 22:02:59
+ * @LastEditTime: 2023-02-27 11:10:34
 -->
 <template>
-  <div class="main-header">main-header</div>
+  <div class="main-header">
+    <div class="menu-icon" @click="handleMenuIconClick">
+      <el-icon size="28px">
+        <component :is="isFold ? 'Expand' : 'Fold'" />
+      </el-icon>
+    </div>
+    <div class="content">
+      <div class="breadcrumb">面包屑</div>
+      <personal-info />
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import PersonalInfo from './c-cpns/personal-info.vue'
 
-<style scoped>
+// 记录状态---展开还是折叠
+const isFold = ref(false)
+// 定义事件
+const emit = defineEmits(['foldChange'])
+function handleMenuIconClick() {
+  isFold.value = !isFold.value
+  // 将事件和状态传递给父组件
+  emit('foldChange', isFold.value)
+}
+</script>
+
+<style lang="less" scoped>
 .main-header {
-  color: #000;
+  display: flex;
+  align-items: center;
+  flex: 1;
+  height: 100%;
+
+  .menu-icon {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+  }
+
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    padding: 0 18px;
+  }
 }
 </style>
