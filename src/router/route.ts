076760f35b2ce27be371f416e6a3169f25566f98
StevenWu <1055034062@qq.com>
2023-03-07 17:10:46 +0800

    notfound页面

--- src/router/route.ts
+++ src/router/route.ts
@@ -4,7 +4,7 @@
  * @Autor: StevenWu
  * @Date: 2023-02-26 21:14:13
  * @LastEditors: StevenWu
- * @LastEditTime: 2023-02-26 22:00:33
+ * @LastEditTime: 2023-03-07 17:10:00
  */
 import { createRouter, createWebHashHistory } from 'vue-router'
 
@@ -27,6 +27,10 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/login/login.vue')
+    },
+    {
+      path: '/:pathMatch(.*)',
+      component: () => import('../views/not-found/not-found.vue')
     }
   ]
 })
---
+++ src/views/not-found/not-found.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: StevenWu
+ * @Date: 2023-03-07 17:09:34
+ * @LastEditors: StevenWu
+ * @LastEditTime: 2023-03-07 17:09:42
+-->
+<template>
+  <div class="not-found">
+    <h2>您输入的路径地址不正确, 请联系管理员~</h2>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped>
+.not-found {
+  color: purple;
+}
+</style>
