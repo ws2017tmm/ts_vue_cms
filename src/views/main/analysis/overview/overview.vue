5c9c2c65382b2de5122af0a1c421681ea7246de6
StevenWu <1055034062@qq.com>
2023-03-08 16:34:15 +0800

    国际化补充

--- src/views/main/analysis/overview/overview.vue
+++ src/views/main/analysis/overview/overview.vue
@@ -4,25 +4,29 @@
  * @Autor: StevenWu
  * @Date: 2023-02-27 11:30:13
  * @LastEditors: StevenWu
- * @LastEditTime: 2023-03-08 16:21:22
+ * @LastEditTime: 2023-03-08 16:33:40
 -->
 <template>
   <div class="overview">
     overview
     <el-button type="primary" v-debounce="[reset]"> tset </el-button>
-    <el-button type="primary" @click="test">
+    <el-button type="primary" @click="changeLang('en')">
       {{ $t('login.userName') }}
     </el-button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { loadLocalMessage } from '@/locales/i18n'
+import { localCache } from '@/utils/cache'
+import { useI18n } from 'vue-i18n'
+const { locale } = useI18n()
 function reset() {
   console.log('----reset')
 }
-function test() {
-  loadLocalMessage()
+
+const changeLang = (lang: string) => {
+  locale.value = lang
+  localCache.setCache('lang', lang)
 }
 </script>
 
